7a2a758630013399a4dc2d460d2ef6c401e32078
Ben Edwards <benny@bennyedwards.net>
2020-06-20 16:53:54 -0700

    mobiles and resets oh my

--- world.c
+++ world.c
@@ -9,12 +9,13 @@
 
 void main()
 {
-  int WIDTH=100;
-  int HEIGHT=100;
+  int WIDTH=25;
+  int HEIGHT=25;
   int VNUM_START=20001; /* must NOT be zero */
   int VNUM_END=WIDTH*HEIGHT+(VNUM_START-1);
   int vnum,n,e,s,w,line_pos;
   int positions[ROWS] = {0, 1, 2, 3, 4, 5};
+  int positions2[ROWS] = {0, 1, 2, 3, 4, 5};
   char name[ROWS][COL] = {"Forest", "Trees", "Woods", "Path", "Deep forest", "Deep woods", "A clearing"};
   char desc[ROWS][COL] = {"Leafy green", "Woodsy and stuff", "Leaves everywhere", "Dirt track", "Quiet undergrowth", "Autumnal crap everywhere", "Babbling brook"};
   char sect[ROWS][COL] = {"2", "3","4","5"};
@@ -26,13 +27,14 @@ void main()
    srand((unsigned) time(&t));
 
 
-  FILE *fp=fopen("mystery.are","w");
+  FILE *fp1=fopen("mystery.1","w");
+  FILE *fp2=fopen("scratch.1","w");
 
   printf("#*********************************#\n\r");
   printf("# GENERATING %3d BY %3d AREA FILE #\n\r",WIDTH,HEIGHT );
   printf("#*********************************#\n\r");
 
-  fprintf(fp,
+  fprintf(fp1,
 "\
 #AREA\n\r\
 mystery.are~\n\r\
@@ -44,8 +46,8 @@ Name Mystery~\n\r\
     VNUM_END);
 
   
-  fprintf(fp,"\n\n#OBJECTS\n#0\n");
-  fprintf(fp,"\n\n#ROOMS\n");
+  fprintf(fp1,"\n\n#OBJECTS\n#0\n");
+  fprintf(fp1,"\n\n#ROOMS\n");
 
   for ( vnum=VNUM_START ; vnum<=VNUM_END  ; vnum++)
    { /* for every room */
@@ -83,16 +85,23 @@ if ( (vnum-VNUM_START+1)%(WIDTH)==1 ) {
   //    printf("vnum %5d WEST  %5d   [line_pos %3d]\n",vnum,w,line_pos);
 }
 
-    fprintf(fp,"#%d\n%s~\n%s\n~\n0 SEB %s\n",vnum,name[positions[rand() % 6]],desc[positions[rand() % 6]],sect[positions[rand() % 4]]); // vnum / room-flags / sector
-    fprintf(fp,"D0\n~\n~\n0 0 %d\n",n);
-    fprintf(fp,"D1\n~\n~\n0 0 %d\n",e);
-    fprintf(fp,"D2\n~\n~\n0 0 %d\n",s);
-    fprintf(fp,"D3\n~\n~\n0 0 %d\n",w);
-    fprintf(fp,"S\n");   
+    fprintf(fp1,"#%d\n%s~\n%s\n~\n0 SEB %s\n",vnum,name[positions[rand() % 6]],desc[positions[rand() % 6]],sect[positions[rand() % 4]]); // vnum / room-flags / sector
+    fprintf(fp1,"D0\n~\n~\n0 0 %d\n",n);
+    fprintf(fp1,"D1\n~\n~\n0 0 %d\n",e);
+    fprintf(fp1,"D2\n~\n~\n0 0 %d\n",s);
+    fprintf(fp1,"D3\n~\n~\n0 0 %d\n",w);
+    fprintf(fp1,"S\n");   
+    fprintf(fp2,"M 0 %s 50 %d 10\n\r",live[positions2[rand() % 6]],vnum);
+    //M <unused> <mobile-vnum> <limit-number> <room-vnum>
   }
-fprintf(fp,"#0\n");
-fprintf(fp,"\n\n#MOBILES\n");
-fprintf(fp,"\n#0\n\n#RESETS\n\n\nS\n\n#SPECIALS\nS\n#$\n");
+fprintf(fp1,"#0\n");
+fprintf(fp1,"\n#RESETS\n");
+fprintf(fp2,"S\n\n\n#MOBILES\n#0\n\n#SPECIALS\nS\n#$\n");
+fclose(fp1);
+fclose(fp2);
+
+printf("Now - run cat *.1 > mystery.are\n\r");
+
 return;
 }
  
